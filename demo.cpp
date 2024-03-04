0d96ee39612d86a825eed376e95b51e99446b488
Qi18 <181250087@nju.edu.com>
2024-03-04 16:05:26 +0800

    feat:优化CPU tbl

--- demo.cpp
+++ demo.cpp
@@ -6,60 +6,62 @@
 #include <random>
 
 using namespace std;
-char*** create3D(int B, int K, int L);
-char** create2D(int B, int M);
-void print3D(char ***arr, int B, int K, int L);
+signed char*** create3D(int B, int K, int L);
+signed char** create2D(int B, int M);
+void print3D(signed char ***arr, int B, int K, int L);
 
 
-char*** calIndexTime(char ***src, char **index, int B, int K, int L) {
+signed char*** calIndexTime(signed char ***src, signed char **index, int B, int K, int L) {
   // src:[B, K, L] + index:[B, M] -> [B, M, L]
   clock_t t1,t2;
+  float val = 0.0;
   t1 = clock();
-  char ***res = create3D(B, 8, L);
+  signed char ***res = create3D(B, 8, L);
   for (int i = 0; i < B; i++) {
     for (int j = 0; j < 8; j++) {
       for (int k = 0; k < L; k++) {
         if (index[i][j] < 0 || index[i][j] >= K) continue;
+        t2 = clock();
         res[i][j][k] = src[i][index[i][j]][k];
+        val += (float)(clock()-t2);
       }
     }
   }
   t2 = clock();
   printf("cpu t = %.8f\n",(float)(t2-t1)/CLOCKS_PER_SEC);
+  printf("cpu t = %.8f\n",val/CLOCKS_PER_SEC);
   return res;
 }
 
 
-char*** calIndexTimeByTBL(char ***src, char **index, int B, int K, int L) {
+signed char*** calIndexTimeByTBL(signed char ***src, signed char **index, int B, int K, int L) {
   clock_t t1,t2;
   t1 = clock();
-  char ***res = create3D(B, 8, L);
+  float val = 0.0;
+  signed char ***res = create3D(B, 8, L);
   for (int i = 0; i < B; i++) {
-    // int8x8_t vdata_index = vld1_dup_s8(index[i]);
-    for (int j = 0; j < L; j++) {
-      for (int k = 0; k < K / 8; k++) {
-        // int8x8_t vdata = vld1_dup_s8(src[i][l] + k * 8 * 8);
-        int8x8_t vdata, vdata_index;
-        for (int l = 0; l < 8; l++) {
-          vdata[l] = src[i][k * 8 + l][j];
-          vdata_index[l] = index[i][l] / 8 == k ? index[i][l] % 8 : -1;
-          // cout << (int)vdata[l] << " ";
-          // cout << (int)vdata_index[l] << endl;
-        }
+    for (int k = 0; k < K / 8; k++) {
+      int8x8_t vdata, vdata_index;
+      vdata_index = vld1_s8(index[i]);
+      // uint32x4_t cmp2 = vcltq_u32(val, vdupq_n_u32(10));
+      for (int l = 0; l < 8; l++) vdata_index[l] = vdata_index[l] / 8 == k ? vdata_index[l] % 8 : -1;
+      for (int j = 0; j < L; j++) {
+        for (int l = 0; l < 8; l++) vdata[l] = src[i][k * 8 + l][j];
+        t2 = clock();
         int8x8_t tbl_2= vtbl1_s8(vdata , vdata_index);
-        for (int l = 0; l < 8; l++) {
-          if (vdata_index[l] != -1) res[i][l][j] = tbl_2[l];
-        }
+        val += (float)(clock()-t2);
+        for (int l = 0; l < 8; l++) if (vdata_index[l] != -1) res[i][l][j] = tbl_2[l];
       }
       // vst1_s8(&(res[i][j][0]), tbl_2);
     }
   }
   t2 = clock();
   printf("cpu tbl t = %.8f\n",(float)(t2-t1)/CLOCKS_PER_SEC);
+  printf("cpu tbl t = %.8f\n",val/CLOCKS_PER_SEC);
   return res;
 }
 
-bool isEqual(char ***arr1, char ***arr2, int K, int M, int L) {
+bool isEqual(signed char ***arr1, signed char ***arr2, int K, int M, int L) {
   for (int i = 0; i < K; i++) {
     for (int j = 0; j < M; j++) {
       for (int k = 0; k < L; k++) {
@@ -84,26 +86,26 @@ void neon_tbl_example()
   }
 }
 
-char*** create3D(int B, int K, int L) {
-  char ***arr = new char**[B];
+signed char*** create3D(int B, int K, int L) {
+  signed char ***arr = new signed char**[B];
   for (int i = 0; i < B; ++i) {
-      arr[i] = new char*[K];
+      arr[i] = new signed char*[K];
       for (int j = 0; j < K; ++j) {
-          arr[i][j] = new char[L];
+          arr[i][j] = new signed char[L];
       }
   }
   return arr;
 }
 
-char** create2D(int B, int M) {
-  char **arr = new char*[B];
+signed char** create2D(int B, int M) {
+  signed char **arr = new signed char*[B];
   for (int i = 0; i < B; ++i) {
-      arr[i] = new char[M];
+      arr[i] = new signed char[M];
   }
   return arr;
 }
 
-void delete3D(char ***arr, int B, int K) {
+void delete3D(signed char ***arr, int B, int K) {
   for (int i = 0; i < B; ++i) {
       for (int j = 0; j < K; ++j) {
           delete[] arr[i][j];
@@ -113,14 +115,14 @@ void delete3D(char ***arr, int B, int K) {
   delete[] arr;
 }
 
-void delete2D(char **arr, int B) {
+void delete2D(signed char **arr, int B) {
   for (int i = 0; i < B; ++i) {
       delete[] arr[i];
   }
   delete[] arr;
 }
 
-void print3D(char ***arr, int B, int K, int L) {
+void print3D(signed char ***arr, int B, int K, int L) {
   cout << "print 3D" << endl;
   for (int i = 0; i < B; ++i) {
       for (int j = 0; j < K; ++j) {
@@ -134,7 +136,7 @@ void print3D(char ***arr, int B, int K, int L) {
   cout << endl;
 }
 
-void print2D(char **arr, int B, int M) {
+void print2D(signed char **arr, int B, int M) {
   cout << "print 2D" << endl;
   for (int i = 0; i < B; ++i) {
       for (int j = 0; j < M; ++j) {
@@ -145,7 +147,7 @@ void print2D(char **arr, int B, int M) {
   cout << endl;
 }
 
-void init3D(char ***arr, int B, int K, int L) {
+void init3D(signed char ***arr, int B, int K, int L) {
   // 使用随机设备生成种子
   std::random_device rd;
   // 使用随机设备生成引擎
@@ -161,7 +163,7 @@ void init3D(char ***arr, int B, int K, int L) {
   }
 }
 
-void init2D(char **arr, int B, int M, int K) {
+void init2D(signed char **arr, int B, int M, int K) {
   for (int i = 0; i < B; ++i) {
       for (int j = 0; j < M; ++j) {
           arr[i][j] = (i * M + j) % K;
@@ -171,12 +173,12 @@ void init2D(char **arr, int B, int M, int K) {
 
 int main() {
   int B = 100, K = 1024, L = 1024, M = 8;
-  char ***src = create3D(B, K, L);
-  char **index = create2D(B, M);
+  signed char ***src = create3D(B, K, L);
+  signed char **index = create2D(B, M);
   init3D(src, B, K, L);
   init2D(index, B, M, K);
-  char ***res2 = calIndexTime(src, index, B, K, L);
-  char ***res1 = calIndexTimeByTBL(src, index, B, K, L);
+  signed char ***res2 = calIndexTime(src, index, B, K, L);
+  signed char ***res1 = calIndexTimeByTBL(src, index, B, K, L);
   isEqual(res1, res2, B, M, L);
   // print3D(src, B, K, L);
   // print2D(index, B, M);
@@ -186,4 +188,5 @@ int main() {
   delete2D(index, B);
   delete3D(res2, B, M);
   delete3D(res1, B, M);
+  // neon_tbl_example();
 }
